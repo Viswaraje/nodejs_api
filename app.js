bd59a639ce3f882d6a2099a11add8d19f5b6d200
viswaraje <v.raje2004@gmail.com>
2024-10-30 22:54:27 +0530

    Initial commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/status", (req, res) => {
-  res.json({ status: "API is running very successfully" });
+  res.json({ status: "API is running very successfullynfnfn" });
 });
 
 const PORT = process.env.PORT || 3000;
